5c957e38ca49d6688c5927fc0352e94b42ac2d61
samhway <83246475+samhway@users.noreply.github.com>
2022-04-04 17:10:18 -0600

    added missing fields to data page

--- CarCrash/Controllers/HomeController.cs
+++ CarCrash/Controllers/HomeController.cs
@@ -25,21 +25,35 @@ namespace CarCrash.Controllers
         public IActionResult Data()
         {
             var Crashes = repo.Crashes.ToList();
-            //List<Location> Locations = repo.Locations.ToList();
-            //List<string> locations = new List<string>();
+            List<Location> Locations = repo.Locations.ToList();
+            List<Road> Roads = repo.Roads.ToList();
+            Dictionary<int, string> locations = new Dictionary<int, string>();
+            Dictionary<int, string> roads = new Dictionary<int, string>();
 
-            //foreach (Crash c in Crashes) 
-            //{
-            //    foreach (Location l in Locations)
-            //    {
-            //        if (l.LOCATION_ID == c.LOCATION_ID)
-            //        {
-            //            locations.Add(l.CITY + ", " + l.COUNTY_NAME);
-            //        }
-            //    }
-            //}
+            foreach (Crash c in Crashes)
+            {
+                foreach (Location l in Locations)
+                {
+                    if (l.LOCATION_ID == c.LOCATION_ID)
+                    {
+                        locations[l.LOCATION_ID] =  l.CITY + ", " + l.COUNTY_NAME;
+                    }
+                }
+            }
 
-            //ViewBag.locations = locations;
+            foreach (Crash c in Crashes)
+            {
+                foreach (Road r in Roads)
+                {
+                    if (r.ROAD_ID == c.ROAD_ID)
+                    {
+                        roads[r.ROAD_ID] = r.MAIN_ROAD_NAME;
+                    }
+                }
+            }
+
+            ViewBag.locations = locations;
+            ViewBag.roads = roads;
             return View(Crashes);
         }
         public IActionResult Prediction()
